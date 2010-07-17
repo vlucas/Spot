b5b6d626ff17a219e77fd7a471f348287ffac57b
Vance Lucas <vance@vancelucas.com>
2010-07-17 12:26:18 -0500

    Tests > Query: Test coverage for query conditional syntax

--- tests/Test/Query.php
+++ tests/Test/Query.php
@@ -45,4 +45,93 @@ class Test_Query extends PHPUnit_Framework_TestCase
         $this->assertTrue($posts instanceof Spot_Query);
 		$this->assertTrue($posts->execute() instanceof Spot_Entity_Collection);
 	}
+	
+	public function testOperatorNone()
+	{
+		$mapper = test_spot_mapper();
+		$post = $mapper->first('Entity_Post', array('status' => 2));
+		$this->assertEquals(2, $post->status);
+	}
+
+	// Equals
+	public function testOperatorEq()
+	{
+		$mapper = test_spot_mapper();
+		$post = $mapper->first('Entity_Post', array('status =' => 2));
+		$this->assertEquals(2, $post->status);
+		$post = $mapper->first('Entity_Post', array('status :eq' => 2));
+		$this->assertEquals(2, $post->status);
+	}
+	
+	// Less than
+	public function testOperatorLt()
+	{
+		$mapper = test_spot_mapper();
+		$this->assertEquals(4, $mapper->all('Entity_Post', array('status <' => 5))->count());
+		$this->assertEquals(4, $mapper->all('Entity_Post', array('status :lt' => 5))->count());
+	}
+
+	// Greater than
+	public function testOperatorGt()
+	{
+		$mapper = test_spot_mapper();
+		$this->assertFalse($mapper->first('Entity_Post', array('status >' => 10)));
+		$this->assertFalse($mapper->first('Entity_Post', array('status :gt' => 10)));
+	}
+
+	// Greater than or equal to
+	public function testOperatorGte()
+	{
+		$mapper = test_spot_mapper();
+
+		$this->assertEquals(6, $mapper->all('Entity_Post', array('status >=' => 5))->count());
+		$this->assertEquals(6, $mapper->all('Entity_Post', array('status :gte' => 5))->count());
+	}
+
+	public function testArrayDefaultIn()
+	{
+		$mapper = test_spot_mapper();
+		$post = $mapper->first('Entity_Post', array('status' => array(2)));
+		$this->assertEquals(2, $post->status);
+	}
+
+	public function testArrayInSingle()
+	{
+		$mapper = test_spot_mapper();
+
+		// Numeric
+		$post = $mapper->first('Entity_Post', array('status :in' => array(2)));
+		$this->assertEquals(2, $post->status);
+
+		// Alpha
+		$post = $mapper->first('Entity_Post', array('status :in' => array('a')));
+		$this->assertFalse($post);
+	}
+
+	public function testArrayNotInSingle()
+	{
+		$mapper = test_spot_mapper();
+		$post = $mapper->first('Entity_Post', array('status !=' => array(2)));
+		$this->assertFalse($post->status == 2);
+		$post = $mapper->first('Entity_Post', array('status :not' => array(2)));
+		$this->assertFalse($post->status == 2);
+	}
+
+	public function testArrayMultiple()
+	{
+		$mapper = test_spot_mapper();
+		$posts = $mapper->all('Entity_Post', array('status' => array(3,4,5)));
+		$this->assertEquals(3, $posts->count());
+		$posts = $mapper->all('Entity_Post', array('status :in' => array(3,4,5)));
+		$this->assertEquals(3, $posts->count());
+	}
+
+	public function testArrayNotInMultiple()
+	{
+		$mapper = test_spot_mapper();
+		$posts = $mapper->all('Entity_Post', array('status !=' => array(3,4,5)));
+		$this->assertEquals(7, $posts->count());
+		$posts = $mapper->all('Entity_Post', array('status :not' => array(3,4,5)));
+		$this->assertEquals(7, $posts->count());
+	}
 }
