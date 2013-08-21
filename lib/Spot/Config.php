442035454ddff8b10d39d3eb3c249797470964ed
Matt Goodwin <matt.goodwin491@gmail.com>
2013-08-21 13:56:56 +0100

    Remove autoload from Config.php and add into init.php to autoload Spot files for testing

--- lib/Spot/Config.php
+++ lib/Spot/Config.php
@@ -138,16 +138,3 @@ class Config implements \Serializable
     {
     }
 }
-
-/**
- * Path trickery designed to find composer autoloader and require
- */
-$vendorPos = strpos(__DIR__, 'vendor/vlucas/spot');
-if($vendorPos !== false) {
-  // Package has been cloned within another composer package, resolve path to autoloader
-  $vendorDir = substr(__DIR__, 0, $vendorPos) . 'vendor/';
-  $loader = require $vendorDir . 'autoload.php';
-} else {
-  // Package itself (cloned standalone)
-  $loader = require __DIR__.'/../../vendor/autoload.php';
-}
